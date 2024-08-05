16348af84a9ba83af07bdfe8dea67a812ee014e1
CodingManoj <CodingManoj@gmail.com>
2024-08-04 21:00:18 -0400

    Auto Push

--- 002-escSeqCharacters.sh
+++ 002-escSeqCharacters.sh
@@ -10,3 +10,7 @@ echo Line2
 
 echo -e "Line3\nLine4"
 echo -e "DevOps\tWith\tAWS\tTraining"
+
+# Whenever you're using special characters , always enclose them in DOUBLE QUOTES
+# " "  : Double Quotes 
+# ' '  : Single Quotes
