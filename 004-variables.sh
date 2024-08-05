03a688efa33a5ec33cba4fe98594da81b3e977d4
CodingManoj <CodingManoj@gmail.com>
2024-08-04 21:25:20 -0400

    Auto Push

--- 004-variables.sh
+++ 004-variables.sh
@@ -15,28 +15,27 @@
 # In bash, everything will be considered as string by default.
 
 a=10                                # ( a is key and 10 is the value )
-b=1.5
+b=1.5                               # Hardcoding the values 
 name=mike 
 
 # How to access the variable ? 
-
 echo a
 echo b
 
 # $ is a special variable and this helps you in printing the value of the variable 
 # Using $ we can access the value of variable 
-
 echo -e "Value of a is \e[32m $a \e[0m"
 echo -e "Value of b is \e[32m $b \e[0m"
 
 # Whenever you're using special variables, single quote is going to nullify the speciality of the special varaible
-
 echo ${a}
 echo $a 
 
 echo "${a}"
 echo '${a}'    
 
-### If try to print a variable that is not declared, what will happen, bash considers thas a null value 
+# Printing a variable, wher ethe value of it is not declared , what will happen ? It will print null.
+echo "The value of the variable d is ${d}"
 
-echo "value of z is $z"
+# rm -rf /data/prod/${APPDATA} ====> /data/prod/shipping
+# rm -rf /data/prod/${APPDATA} ====> /data/prod
---
+++ 005-dynamicVars.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+DATE="05AUG2024"
+
+echo "Todays date is $DATE"
