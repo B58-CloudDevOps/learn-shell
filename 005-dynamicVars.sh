415e5150e5ffcde4c7ff1d4d2b41dc3de792c434
CodingManoj <CodingManoj@gmail.com>
2024-08-04 21:27:01 -0400

    Auto Push

--- 005-dynamicVars.sh
+++ 005-dynamicVars.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 
-# DATE="03AUG2024"
-
-DATE=$(date +%F)          
+# DATE="03AUG2024"                # This is called hardCoding 
+ 
+DATE=$(date +%F)                  # This is how we can dynamically fetch the values
 
 echo "Todays date is $DATE"
+
+echo "Script Executed By Mike"
