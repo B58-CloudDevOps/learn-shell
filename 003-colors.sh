bf3215209b91c3fe8190a2318c706d629a8a8f96
CodingManoj <CodingManoj@gmail.com>
2024-08-04 21:50:19 -0400

    Auto Push

--- 003-colors.sh
+++ 003-colors.sh
@@ -1,14 +1,13 @@
 #!/bin/bash 
 
-
 # Each and every color you see on terminal will have a color code and we need to use that code baesd on our need.
 # Colors       Foreground          Background
-# Red               31                  41
-# Green             32                  42
-# Yellow            33                  43
-# Blue              34                  44
-# Magenta           35                  45
-# Cyan              36                  46
+# Red              31                  41
+# Green            32                  42
+# Yellow           33                  43
+# Blue             34                  44
+# Magenta          35                  45
+# Cyan             36                  46
 
 # Syntax to print colors 
 
@@ -28,7 +27,6 @@ echo -e "\e[35m This shows Magneta color \e[0m"
 # echo -e "\e[43;31m I am printing Text with a backGround Color \e[0m"
 
 echo -e "\e[46;31m I am printing Text with a backGround color \e[0m"
-
 # Since in bash and shell, there are lot of versions, both of the syntaxes are valid, in case if anyone doesn't work, prefer to use the other one.
 # echo $a 
 # echo ${a}
