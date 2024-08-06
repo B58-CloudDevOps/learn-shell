0d6cf764582f6f5de74bbaaf2325cb5bc95e8426
CodingManoj <CodingManoj@gmail.com>
2024-08-05 21:13:40 -0400

    Auto Push

--- 013-if.sh
+++ 013-if.sh
@@ -9,10 +9,28 @@ ACTION=$1
 #     exit 0
 # fi  
 
+
+# Demo on if else 
+# if [ "$ACTION" = "start" ] ; then 
+#     echo -e "\e[32m Backed is starting \e[0m"
+#     exit 0
+# else 
+#     echo -e "\e[31m start is valid option \e[0m"
+#     exit 3
+# fi  
+
+# Demo on else 
 if [ "$ACTION" = "start" ] ; then 
     echo -e "\e[32m Backed is starting \e[0m"
     exit 0
-else 
-    echo -e "\e[31m start is valid option \e[0m"
+
+else if  [ "$ACTION" = "stop " ] ; then 
+    echo -e "\e[31m Backed is stopping \e[0m"
     exit 3
+
+else if  [ "$ACTION" = "restart " ] ; then 
+    echo -e "\e[31m Backed is restarting \e[0m"
+    exit 3
+else 
+    echo -e "\e[31m Entered Option is invalid --> Valid Options are start or stop or restart only \e[0m"
 fi  
