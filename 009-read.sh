27d1432677ee7e9c4e2229fc6ea007997ba08153
CodingManoj <CodingManoj@gmail.com>
2024-08-05 20:41:54 -0400

    Auto Push

--- 009-read.sh
+++ 009-read.sh
@@ -2,4 +2,7 @@
 
 echo "This script will proceed by taking input from the user"
 read -p "Enter Value Of A:" A
+read -p "Enter Value Of B:" B
 echo "Value of A is $A" 
+echo "Value of B is $B" 
+echo "Sum of A + B  = $(A+B)"
