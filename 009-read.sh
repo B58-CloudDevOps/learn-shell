962ca2ca0b39e1c20e4bc61875838393b9e7303f
CodingManoj <CodingManoj@gmail.com>
2024-08-05 20:41:12 -0400

    Auto Push

--- 009-read.sh
+++ 009-read.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
-echo "This script will proceed by taking input from the user
+echo "This script will proceed by taking input from the user"
 read -p "Enter Value Of A:" A
 echo "Value of A is $A" 
